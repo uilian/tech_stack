6df9176d13807b906e42e6964717169c0143eb6f
Uilian Souza <uilian@gmail.com>
2017-09-25 08:12:33 -0300

    Basic application structure

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import { View } from 'react-native';
 imporrt { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
 
 const App = () => {
   return (
-    <Provider store={createStore(reducers)}>
-      <View />
+    <Provider store={createStore(reducers)} >
+      <View>
+        <Header>
+          Tech Stack
+        </Header>
+      </View>
     </Provider>
   );
-}
+};
 
 
 export default App;
